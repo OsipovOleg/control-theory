4bacbf3db9f0018d79d146744210cd06a18d1085
OsipovOleg <oleg.alex.osipov@gmail.com>
2021-05-04 23:26:35 +0400

    fix build script

--- sources/build_clean.sh
+++ sources/build_clean.sh
@@ -1,9 +1,13 @@
 rm *.pdf
 for doc in *.tex; do pdflatex $doc; done
 for doc in *.tex; do pdflatex $doc; done
+rm ../pract/*
+cp -r *.pdf  ../pract
+
 rm *.fls
 rm *aux
 rm *.gz
 rm *.fdb_latexmk
 rm *.log
 rm *.dvi
+rm *.pdf
